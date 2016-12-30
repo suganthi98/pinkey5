fddbb394da7c2ad50d95ba65817bb7717ef6fa54
suganthi98 <suganthimathu97@gmail.com>
2016-12-30 09:50:50 +0530

    Add files via upload

---
+++ sumfor.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n,sum=0,i;
+printf("enter the n number");
+scanf("%d",&n);
+for(i=1;i<=n;i++);
+{
+sum=sum+i;
+}
+printf("sum of natuarl number %d",sum);
+return 0;
+}
